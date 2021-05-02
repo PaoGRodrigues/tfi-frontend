a060af147afbd8a419eed413d5b57679eb2e83dc
PaoRodrigues <paola.rodrigues@mercadolibre.com>
2021-05-02 19:12:36 -0300

    Add DS_Store to gitignore and update script to connect to Raspi with ssh key

--- ansible/run-playbook.sh
+++ ansible/run-playbook.sh
@@ -1,2 +1,2 @@
 docker build -t ansible .
-docker run -it -v "$PWD":/ansible -v "$PWD/known_hosts":/root/.ssh/known_hosts ansible ansible-playbook $1
+docker run -it -v "$PWD":/ansible ansible ansible-playbook $1
