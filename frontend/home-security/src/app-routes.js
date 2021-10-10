183dfcc5a650f5170f716c18fa1293e2e9cee231
PaoRodrigues <paola.rodrigues@mercadolibre.com>
2021-10-10 20:52:47 -0300

    Initial frontend app

---
+++ frontend/home-security/src/app-routes.js
@@ -0,0 +1,10 @@
+import { withNavigationWatcher } from './contexts/navigation';
+
+const routes = [];
+
+export default routes.map(route => {
+  return {
+    ...route,
+    component: withNavigationWatcher(route.component)
+  };
+});
