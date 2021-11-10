9234fb392b31dd939593e2446ce1418b7ab74e3e
PaoGRodrigues <paogrodrigues@gmail.com>
2021-11-10 01:35:01 -0300

    Add request to localhost to fetch the data

--- frontend/home-security/src/pages/home/home.js
+++ frontend/home-security/src/pages/home/home.js
@@ -1,41 +1,43 @@
 import React from 'react';
+import 'devextreme/dist/css/dx.light.css';
+
 import DataGrid from 'devextreme-react/data-grid';
-
-const columns = ['device', 'ip', 'details' ];
-
-const devices = [
-    {
-        device:'Celular',
-        ip: '192.168.4.5',
-        details: 'Test',
+import CustomStore from 'devextreme/data/custom_store';
+import 'whatwg-fetch';
+ 
+function handleErrors(response) {
+    if (!response.ok) {
+        throw Error(response.statusText);
+    }
+    return response;
+}
+ 
+const customDataSource = new CustomStore({
+    load: () => {
+        return fetch(`http://localhost:8080/devices`)
+            .then(handleErrors)
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    data: response.data,
+                };
+            })
+            .catch(() => { throw 'Network error' });
     },
-    {
-        device:'Laptop',
-        ip: '192.168.4.10',
-        details: 'Test1',
-    },
-    {
-        device:'Tv',
-        ip: '192.168.4.15',
-        details: 'Test2',
-    },
-];
-
+});
+ 
 export default () => {
     return (
         <div style={{ overflowY: 'scroll', height: 'calc(100vh - 127px)' }}>
-        <React.Fragment>
+        <React.Fragment style={{align: 'right'}}>
             <h2>Resumen</h2>
             <div className={'content-block dx-card responsive-paddings'}>
-            <h2>
-                Dispositivos
-            </h2>
-            <DataGrid
-                dataSource={devices}
-                keyExpr="ip"
-                defaultColumns={columns}
-                showBorders={true}
-            />
+                <h2>Dispositivos</h2>
+                <DataGrid
+                    dataSource={customDataSource}
+                    // defaultColumns={columns}
+                    showBorders={true}
+                />
             </div>
         </React.Fragment>
         </div>
