3c5aed0a6e5479d47f436bca85e793af3bf5b0c4
PaoGRodrigues <paogrodrigues@gmail.com>
2021-11-22 19:22:22 -0300

    First approach of Traffic chart

---
+++ frontend/home-security/src/pages/home/data.js
@@ -0,0 +1,18 @@
+export const bytesValue =[
+    {value: 'bytes', name: 'Bytes'}
+]
+
+export const bytesPerHour = [{
+    bytes: 234568,
+    
+    datetime: new Date(2021, 1, 22),
+  },
+  {
+    bytes: 134568,
+    datetime: new Date(2021, 2, 22),
+  },
+  {
+    bytes: 234568,
+    datetime: new Date(2021, 3, 22),
+  }
+]
--- frontend/home-security/src/pages/home/home.js
+++ frontend/home-security/src/pages/home/home.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import 'devextreme/dist/css/dx.light.css';
-
 import DataGrid from 'devextreme-react/data-grid';
 import CustomStore from 'devextreme/data/custom_store';
 import 'whatwg-fetch';
+import {
+    Chart,
+    CommonSeriesSettings,
+    Series,
+    ArgumentAxis,
+    Grid,
+    Crosshair,
+    Export,
+    Legend,
+    Point,
+    Label,
+    Font,
+    Title,
+    Subtitle,
+    Tooltip,
+  } from 'devextreme-react/chart';
+import { bytesPerHour, bytesValue } from './data.js';
+
  
 function handleErrors(response) {
     if (!response.ok) {
@@ -29,17 +46,74 @@ const customDataSource = new CustomStore({
 export default () => {
     return (
         <div style={{ overflowY: 'scroll', height: 'calc(100vh - 127px)' }}>
-        <React.Fragment style={{align: 'right'}}>
-            <h2>Resumen</h2>
+            <React.Fragment style={{align: 'right'}}>
+                <h2>Resumen</h2>
+                <div className={'content-block dx-card responsive-paddings'}>
+                    <h2>Dispositivos</h2>
+                    <DataGrid
+                        dataSource={customDataSource}
+                        // defaultColumns={columns}
+                        showBorders={true}
+                        />
+                </div>
+            </React.Fragment>
+          
             <div className={'content-block dx-card responsive-paddings'}>
-                <h2>Dispositivos</h2>
-                <DataGrid
-                    dataSource={customDataSource}
-                    // defaultColumns={columns}
-                    showBorders={true}
-                />
+                <React.Fragment style={{align: 'right'}}>
+                    <Chart
+                        id="chart"
+                        dataSource={bytesPerHour}
+                        palette="Harmony Light"
+                    >
+                    <CommonSeriesSettings
+                    type="spline"
+                    argumentField="datetime"
+                    >
+                    <Point hoverMode="allArgumentPoints" />
+                    </CommonSeriesSettings>
+                    {
+                    bytesValue.map((item) => <Series
+                        key={item.value}
+                        valueField={item.value}
+                        name={item.name}
+                        />)
+                    }
+                    <ArgumentAxis
+                    valueMarginsEnabled={false}
+                    discreteAxisDivisionMode="crossLabels"
+                    >
+                    <Grid visible={true} />
+                    </ArgumentAxis>
+                    <Crosshair
+                    enabled={true}
+                    color="#949494"
+                    width={3}
+                    dashStyle="dot"
+                    >
+                    <Label
+                        visible={true}
+                        backgroundColor="#949494"
+                    >
+                        <Font
+                        color="#fff"
+                        size={12}
+                        />
+                    </Label>
+                    </Crosshair>
+                    <Legend
+                    verticalAlignment="bottom"
+                    horizontalAlignment="center"
+                    itemTextPosition="bottom"
+                    equalColumnWidth={true}
+                    />
+                    <Title text="Cantidad de Bytes por día">
+                    <Subtitle text="(bytes)" />
+                    </Title>
+                    <Export enabled={true} />
+                    <Tooltip enabled={true} />
+                    </Chart>
+                </React.Fragment>
             </div>
-        </React.Fragment>
         </div>
     );
-};
+}
