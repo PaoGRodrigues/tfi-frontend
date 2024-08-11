a95159c4fcce3cf8e5b92d8afb5e4cf713100b83
PaoRodrigues <paogrodrigues@gmail.com>
2024-08-11 20:42:47 -0300

    Delete user features

--- frontend/home-security/src/App.js
+++ frontend/home-security/src/App.js
@@ -4,25 +4,12 @@ import './themes/generated/theme.additional.css';
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
 import './dx-styles.scss';
-import LoadPanel from 'devextreme-react/load-panel';
 import { NavigationProvider } from './contexts/navigation';
-import { AuthProvider, useAuth } from './contexts/auth';
 import { useScreenSizeClass } from './utils/media-query';
 import Content from './Content';
-import UnauthenticatedContent from './UnauthenticatedContent';
 
 function App() {
-  const { user, loading } = useAuth();
-
-  if (loading) {
-    return <LoadPanel visible={true} />;
-  }
-
-  if (user) {
-    return <Content />;
-  }
-
-  return <UnauthenticatedContent />;
+  return <Content />;
 }
 
 export default function Root() {
@@ -30,13 +17,11 @@ export default function Root() {
 
   return (
     <Router>
-      <AuthProvider>
-        <NavigationProvider>
-          <div className={`app ${screenSizeClass}`}>
-            <App />
-          </div>
-        </NavigationProvider>
-      </AuthProvider>
+      <NavigationProvider>
+        <div className={`app ${screenSizeClass}`}>
+          <App />
+        </div>
+      </NavigationProvider>
     </Router>
   );
 }
